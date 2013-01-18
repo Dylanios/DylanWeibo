ddc4c53b037bd1cb7becfbebc69504a722f9183d
Dylan <dylan@DylantekiMacBook-Pro.local>
2013-01-18 16:17:40 +0800

    Initial Commit

---
+++ DylanWeibo/DAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DAppDelegate.h
+//  DylanWeibo
+//
+//  Created by Dylan on 13-1-18.
+//  Copyright (c) 2013年 Dylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DylanWeibo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DylanWeibo
+//
+//  Created by Dylan on 13-1-18.
+//  Copyright (c) 2013年 Dylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DAppDelegate class]));
+    }
+}
